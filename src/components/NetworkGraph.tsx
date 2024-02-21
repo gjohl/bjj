b11da81ce77733eca537e9784f071eb3199e7a46
Gurpreet Johl <gurpreetjohl@gmail.com>
2024-02-21 22:43:20 +0000

    GJ-95 Refactoring and making things prettier (#4)
    * Split out position-data.json
    
    * Add some position-data
    
    * Some nice pretty tweaks
    
    * Some tidy up
    
    * Add data parser
    
    * Update readme

--- src/components/NetworkGraph.tsx
+++ src/components/NetworkGraph.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { ForceGraph2D } from 'react-force-graph';
 
 type Props = {
@@ -7,21 +7,46 @@ type Props = {
 
 const NetworkGraph: React.FC<Props> = (props : Props) => {
     const { graphData } = props;
+    // const fgRef = useRef();
+
+    const paintRing = useCallback((node: any, ctx: any) => {
+        ctx.beginPath();
+        ctx.arc(node.x, node.y, 10, 0, 2 * Math.PI, false);
+        // ctx.fillStyle = "blue";
+        ctx.fill();
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "white";
+        ctx.fillText(node.id, node.x, node.y);
+      }, []);
+
     return (
         <ForceGraph2D 
         graphData={graphData}
-        nodeRelSize={5}
-        backgroundColor='white'
-        height={500}
-        width={500}
-        linkWidth={15}
-        // minZoom={0.01}
-        // linkColor='red'
+        nodeAutoColorBy={"group"}
         enableNodeDrag={false}
-        linkCurvature={0.3}
+
+        //  For debugging UI
+        // backgroundColor='white'
+        // height={500}
+        // width={500}
+
+        // Zoom-to-fit
+        // ref={fgRef}
+        // onEngineStop={() => fgRef.current?.zoomToFit(400)}
+
+        // Link attributes        
+        linkWidth={15}
+        linkCurvature={"curvature"}
         linkDirectionalArrowLength={10}
         linkDirectionalArrowRelPos={0.5}
-        linkDirectionalParticles={4}
+        linkDirectionalParticles={3}
+
+        // Text in nodes
+        nodeRelSize={0}
+        autoPauseRedraw={false}
+        nodeCanvasObjectMode={() => 'before' }
+        nodeCanvasObject={paintRing}
         />
     )
 }
---
+++ src/data/data-parser.ts
@@ -0,0 +1,15 @@
+import * as inputData from './position-data.json';
+
+
+type InputDataType = {
+     nodes: { id: string; val: number; }[]; 
+     links: { source: string; target: string; }[]
+}
+
+const dataParser = (inputData: InputDataType) => {
+    return inputData
+}
+
+const graphData = dataParser(inputData);
+
+export default graphData;
