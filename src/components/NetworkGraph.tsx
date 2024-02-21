208491869069a56ea015db6e94e746acdceec599
Gurpreet Johl <gurpreetjohl@gmail.com>
2024-02-21 16:59:45 +0000

    GJ-97 Refactor react components (#3)
    * Network graph working from react App component
    
    * Remove unnecessary boilerplate
    
    * Graph inside App component
    
    * Split out NetworkGraph component

---
+++ src/components/NetworkGraph.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ForceGraph2D } from 'react-force-graph';
+
+type Props = {
+    graphData: { nodes: { id: string; val: number; }[]; links: { source: string; target: string; }[]}
+}
+
+const NetworkGraph: React.FC<Props> = (props : Props) => {
+    const { graphData } = props;
+    return (
+        <ForceGraph2D 
+        graphData={graphData}
+        nodeRelSize={5}
+        backgroundColor='white'
+        height={500}
+        width={500}
+        linkWidth={15}
+        // minZoom={0.01}
+        // linkColor='red'
+        enableNodeDrag={false}
+        linkCurvature={0.3}
+        linkDirectionalArrowLength={10}
+        linkDirectionalArrowRelPos={0.5}
+        linkDirectionalParticles={4}
+        />
+    )
+}
+
+export default NetworkGraph;
