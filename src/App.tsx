1b73aa54125e558a4c3eafd00a5a036a5b9c3bbd
Gurpreet Johl <gurpreetjohl@gmail.com>
2024-02-21 16:06:07 +0000

    Graph inside App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { useMemo, useState, useCallback } from 'react'
+// import { useMemo, useState, useCallback } from 'react'
 import './App.css'
 import { ForceGraph2D } from 'react-force-graph';
 
 
 const App = () => {
   const mockData = {
-    nodes: [{id: 1}, {id: 2}, {id:3}],
-    links: [{from: 1, to: 3}]
+    nodes: [
+        {id: "id1", "val": 1 }, 
+        {id: "id2", "val": 2 }, 
+        {id: "id3", "val": 3 }
+    ],
+    links: [
+        {source: "id1", target: "id3"},
+        {source: "id1", target: "id3"},
+        {source: "id1", target: "id2"},
+    ]
   }
 
   return (
@@ -21,7 +29,20 @@ const App = () => {
       <div>
         <p>
         <ForceGraph2D 
-        graphData={mockData}/>
+        graphData={mockData}
+        nodeRelSize={5}
+        backgroundColor='white'
+        height={500}
+        width={500}
+        linkWidth={15}
+        // minZoom={0.01}
+        // linkColor='red'
+        enableNodeDrag={false}
+        linkCurvature={0.3}
+        linkDirectionalArrowLength={10}
+        linkDirectionalArrowRelPos={0.5}
+        linkDirectionalParticles={4}
+        />
         </p>
       </div>
     </>
