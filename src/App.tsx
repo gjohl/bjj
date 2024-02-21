f27b2c6d296ad9e6c350e51f827f6c77a92a565d
Gurpreet Johl <gurpreetjohl@gmail.com>
2024-02-21 15:38:05 +0000

    Network graph working from react App component

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useMemo, useState, useCallback } from 'react'
+import './App.css'
+import { ForceGraph2D } from 'react-force-graph';
+
+
+const App = () => {
+  const mockData = {
+    nodes: [{id: 1}, {id: 2}, {id:3}],
+    links: [{from: 1, to: 3}]
+  }
+
+  return (
+    <>
+      <h1>BJJ Notes</h1>
+      {/* <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div> */}
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      
+      <div>
+        <p>
+        <ForceGraph2D 
+        graphData={mockData}/>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
