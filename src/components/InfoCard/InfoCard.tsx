25f5a318666dae939568416d41e8cf11de1e04be
Gurpreet Johl <gurpreetjohl@gmail.com>
2024-02-23 10:55:37 +0000

    GJ-103 Info detail panel component (#7)
    * Add info details panel
    
    * Align info items

--- src/components/InfoCard/InfoCard.tsx
+++ src/components/InfoCard/InfoCard.tsx
@@ -7,36 +7,77 @@ const InfoCard: React.FC<any> = (props) => {
 
     return (
         <>
-            <div className='infoTitle'>
-                SIDE MOUNT
+            <div>
+                <div className='infoTitle'>
+                    SIDE MOUNT
+                </div>
+
+                <div className="subtitleContainer">
+                    <div className='infoSubsectionContainer'>
+                        <div className='infoSubtitle'>
+                            Sweeps
+                        </div>
+                        <div className='infoListItems'>
+                            <ul>
+                                {sweepList.map((item: string) => <li>{item}</li>)}
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='infoSubsectionContainer'>
+                        <div className='infoSubtitle'>
+                            Escapes
+                        </div>
+                        <div className='infoListItems'>
+                            <ul>
+                                {escapeList.map((item: string) => <li>{item}</li>)}
+                            </ul>
+                        </div>
+                    </div>
+
+
+                    <div className='infoSubsectionContainer'>
+                        <div className='infoSubtitle'>
+                            Submissions
+                        </div>
+                        <div className='infoListItems'>
+                            <ul>
+                                {submissionList.map((item: string) => <li>{item}</li>)}
+                            </ul>
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
 
-            <div className='infoSubtitle'>
-                Sweeps
-            </div>
-            <div className='infoListItems'>
-                <ul>
-                    {sweepList.map((item: string) => <li>{item}</li>)}
-                </ul>
+            <div className='infoDetailPanel'>
+                <div className='infoDetailTitle'>
+                    MOVE DETAILS
+                </div>
+
+                <div className='subtitleContainer'>
+                <div className='infoSubsectionContainer'>
+                    <div className='infoSubtitle'>
+                        Gurp's Notes
+                    </div>
+                    <div className='infoDetailDescription'>
+                        Gurp's notes on the selected move.
+                    </div>
+                </div>
+
+                <div className='infoSubsectionContainer'>
+                    <div className='infoSubtitle'>
+                        Related links
+                    </div>
+
+                    <div className='infoDetailRelatedLinks'>
+                        <a href="http://w3schools.com">Link example</a>
+                    </div>
+                </div>
+                </div>
             </div>
 
-            <div className='infoSubtitle'>
-                Escapes
-            </div>
-            <div className='infoListItems'>
-                <ul>
-                    {escapeList.map((item: string) => <li>{item}</li>)}
-                </ul>
-            </div>
-
-            <div className='infoSubtitle'>
-                Submissions
-            </div>
-            <div className='infoListItems'>
-                <ul>
-                    {submissionList.map((item: string) => <li>{item}</li>)}
-                </ul>
-            </div>
         </>
     )
 }
