46c740529cb26f7950d7a554502bbf88da696034
Gurpreet Johl <gurpreetjohl@gmail.com>
2024-02-24 11:19:04 +0000

    Sweeps are dynamic

--- src/components/InfoCard/InfoCard.tsx
+++ src/components/InfoCard/InfoCard.tsx
@@ -1,15 +1,41 @@
 import './InfoCard.css';
 import React from "react";
 
+const ListContents = (props: any) => {
+    const { inputList } = props;
+    if (inputList.length > 0) {
+        return <ul>
+            {inputList.map((item: any) => <li>{item.name}</li>)}
+        </ul>
+    }
+    return "Gurp needs to learn more..."
+
+}
+
 
 const InfoCard: React.FC<any> = (props) => {
     const { node, sweepList, escapeList, submissionList } = props;
     const titleName = node ? node.name : 'Select a node';
-    
-    // console.log(node);
-    // const links = node ? node.links : [];
-    // const linkNames = links ? links.map((link: any) => {link.name}) : [];
-    // console.log(linkNames);
+
+    let linkNames = [];
+    let linkDescriptions = [];
+    let linkRelatedLinks = [];
+    let sweeps = []
+    let escapes = [];
+    let submissions = [];
+    console.log(node);
+
+    if (node && node.links) {
+        linkNames = node.links.map((link: any) => link.name);
+        linkDescriptions = node.links.map((link: any) => link.description);
+        linkRelatedLinks = node.links.map((link: any) => link.relatedLinks);
+        sweeps = node.links.filter((link: any) => link.transitionType === 'sweep');
+        escapes = node.links.filter((link: any) => link.transitionType === 'escape');
+        submissions = node.links.filter((link: any) => link.transitionType === 'submission');
+        console.log(submissions);
+    } else {
+        console.log("Node is null or links array doesn't exist.");
+    }
 
     return (
         <>
@@ -24,9 +50,7 @@ const InfoCard: React.FC<any> = (props) => {
                             Sweeps
                         </div>
                         <div className='infoListItems'>
-                            <ul>
-                                {sweepList.map((item: string) => <li>{item}</li>)}
-                            </ul>
+                            <ListContents inputList={sweeps}/>
                         </div>
                     </div>
 
@@ -63,24 +87,24 @@ const InfoCard: React.FC<any> = (props) => {
                 </div>
 
                 <div className='subtitleContainer'>
-                <div className='infoSubsectionContainer'>
-                    <div className='infoSubtitle'>
-                        Gurp's Notes
-                    </div>
-                    <div className='infoDetailDescription'>
-                        Gurp's notes on the selected move.
-                    </div>
-                </div>
-
-                <div className='infoSubsectionContainer'>
-                    <div className='infoSubtitle'>
-                        Related links
+                    <div className='infoSubsectionContainer'>
+                        <div className='infoSubtitle'>
+                            Gurp's Notes
+                        </div>
+                        <div className='infoDetailDescription'>
+                            Gurp's notes on the selected move.
+                        </div>
                     </div>
 
-                    <div className='infoDetailRelatedLinks'>
-                        <a href="http://w3schools.com">Link example</a>
+                    <div className='infoSubsectionContainer'>
+                        <div className='infoSubtitle'>
+                            Related links
+                        </div>
+
+                        <div className='infoDetailRelatedLinks'>
+                            <a href="http://w3schools.com">Link example</a>
+                        </div>
                     </div>
-                </div>
                 </div>
             </div>
 
