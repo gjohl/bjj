6569becf3d0574c0875b412b0016ba4a6eab7768
Gurpreet Johl <gurpreetjohl@gmail.com>
2024-03-28 19:34:31 +0000

    Fix node colouring

--- src/screens/TaxonomyScreen.tsx
+++ src/screens/TaxonomyScreen.tsx
@@ -18,25 +18,30 @@ const TaxonomyScreen: React.FC<any> = (props) => {
     const [clickNode, setClickNode] = useState(null);
     const [selectedMove, setSelectedMove] = useState(null);
 
+    console.log(highlightNodes);
+
     const paintRing = useCallback((node: Node, ctx: any) => {
         //  muted lavender #2f1d43
         // bright purple #7925D3
         // lilac #BE97C6
 
         const nodeText = node.shortName;
-
-        // Main node
-        ctx.beginPath();
-        ctx.arc(node.x, node.y, NODE_R, 0, 2 * Math.PI, false);
-        ctx.fillStyle = "black";
-        ctx.fill();
-
-        // Add ring just for highlighted nodes
+        // Vary node colour depending on if the node is selected, neighbouring or neither
         ctx.beginPath();
         ctx.arc(node.x, node.y, NODE_R * 1.4, 0, 2 * Math.PI, false);
-        ctx.fillStyle = node === clickNode ? '#7925D3' : '#2f1d43';
+        if (node === clickNode) {
+            // selected node
+            ctx.fillStyle = '#7925D3';
+        } else {
+            if (highlightNodes.has(node)) {
+                // neighbouring nodes
+                ctx.fillStyle = '#BE97C6';
+            } else {
+                // neither
+                ctx.fillStyle = '#2f1d43';
+            }
+        }
         ctx.fill();
-
 
         // Text
         ctx.textAlign = "center";
@@ -79,8 +84,6 @@ const TaxonomyScreen: React.FC<any> = (props) => {
                 <NetworkGraph
                     graphData={graphData}
                     nodeRelSize={NODE_R}
-                    nodeCanvasObjectMode={(node: Node) => highlightNodes.has(node) ? 'before' : 'after'}
-                    // nodeCanvasObjectMode={'replace'}
                     nodeCanvasObject={paintRing}
                     linkWidth={(link: Link) => highlightLinks.has(link) ? 5 : 2}
                     onNodeClick={handleNodeClick}
